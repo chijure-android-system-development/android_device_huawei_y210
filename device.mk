2cdb4532042ea4e5b6188037596332665debfed0
Jhuan Reategui Solis <reateguisolisjm@gmail.com>
2026-01-25 03:01:20 -0500

    Update recovery configuration and add new scripts for Huawei Y210

--- device.mk
+++ device.mk
@@ -2,7 +2,10 @@
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery.fstab:recovery/root/etc/recovery.fstab \
+    $(LOCAL_PATH)/ramdisk/recovery/twrp.fstab:recovery/root/etc/twrp.fstab \
     $(LOCAL_PATH)/ramdisk/recovery/sbin/charge:recovery/root/sbin/charge \
+    $(LOCAL_PATH)/ramdisk/recovery/sbin/linker:recovery/root/sbin/linker \
+    $(LOCAL_PATH)/ramdisk/recovery/sbin/ncm.sh:recovery/root/sbin/ncm.sh \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ramdisk/recovery/res,recovery/root/res)
 
 PRODUCT_PACKAGES += adbd
